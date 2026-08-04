9a3bebe6e5bc8b9b97bf1b3a632e403c46695411
Priyanka <priyankaraghochaten@gmail.com>
2026-08-04 20:21:48 +0530

    03-props

---
+++ 02-components/components/Card.jsx
@@ -0,0 +1,10 @@
+export function Card(){
+    const user = 'priyanka'
+return (         <div className="card">
+            <h1>{user } </h1>
+            <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate laudantium fugit sequi?</h3>
+    
+          </div>
+          )
+}
+export default Card;
---
+++ 03-props/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (<div className="parent">
+    <div className='card'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzdRIoWmVlJz1VZp2gNHWevR6s1AGlwm_Hqfi8p1cwwbjUQethuaShFpmr&s=10" alt="" srcset="" />
+      <h1>Priyanka Raghochatan</h1>
+
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit optio nesciunt similique.</p>
+      <button>view profile</button>
+    </div>
+   
+    </div>
+  )
+}
+
+export default App
