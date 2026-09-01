f6f9c1be7c3b5ef6aada338182cc00aa2189ca58
Priyanka <priyankaraghochaten@gmail.com>
2026-09-01 21:36:17 +0530

    react router basics

--- 15-gallery-project/src/components/Card.jsx
+++ 15-gallery-project/src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
-      <a href={elem.url}>
+      <a href={props.url}>
        <div className='h-40 w-44 bg-white'>
-      <img className='h-full w-full overflow-hidden object-cover' src={elem.download_url} alt="" />
+      <img className='h-full w-full overflow-hidden object-cover' src={props.elem.download_url} alt="" />
     </div>
-     <h2 className='font-bold text-lg'>{elem.author} </h2>
+     <h2 className='font-bold text-lg'>{props.elem.author} </h2>
     </a>
     </div>
   )
---
+++ 16-React-router-DOM/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About us is not related to you </h1>
+    </div>
+  )
+}
+
+export default About
